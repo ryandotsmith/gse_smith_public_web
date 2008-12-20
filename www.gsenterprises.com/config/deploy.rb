3d83afce9c104675d6c77b68bee4385c385bc963
Ryan Smith <this.ryansmith@gmail.com>
2008-12-20 13:25:22 -0600

    changed deploy

--- www.gsenterprises.com/config/deploy.rb
+++ www.gsenterprises.com/config/deploy.rb
@@ -2,18 +2,22 @@ require 'capistrano/recipes/deploy/strategy'
 
     
     # Set your project name here, it's probably best to use the domain name.
-    set :application, "www.gsenterprises.com" 
+    set :application, "gse_smith_public_web" 
+    #set :user, "rsmith"
+    #set :ssh_options, { :forward_agent => true }
+    #set :use_sudo,    false
+    
+    default_run_options[:pty] = true
+    set :repository,  "git@github.com:ryandotsmith/gse_smith_public_web.git"
+    set :scm, "git"
+    set :scm_passphrase, "$m1thert0n" #This is your custom users password
     set :user, "rsmith"
     set :ssh_options, { :forward_agent => true }
-    set :use_sudo,    false
+
     # Roles (You probably won't need to modify these.)
     role :app, "10.0.1.20"
     role :web, "10.0.1.20"
     role :db,  "10.0.1.20", :primary => true
-
-    set :scm, :git
-    set :repository, "git@10.0.1.20:www.gsenterprises.com.git"
-    set :branch, "master"
-    set :deploy_via, :remote_cache
-
+    
     set :deploy_to, "/var/www/#{application}" 
+    
